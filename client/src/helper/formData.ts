848caf252e18f734bfb3082bffcddb70fb36bb8e
Aymanktari <aymanktari31@gmail.com>
2023-04-04 00:39:08 +0100

    formData

---
+++ client/src/helper/formData.ts
@@ -0,0 +1,18 @@
+import { LoginFormValues, RegisterFormValues } from '../interface/user';
+
+const RegisterFormData: RegisterFormValues = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  bYear: new Date().getFullYear(),
+  bMonth: new Date().getMonth() + 1,
+  bDay: new Date().getDate(),
+  gender: ''
+};
+
+const loginFormData: LoginFormValues = {
+  email: '',
+  password: ''
+};
+export { RegisterFormData, loginFormData };
