bfdba0d5a0fe6edce5f2f81ad8dda86dd0ba6f73
Aymanktari <aymanktari31@gmail.com>
2023-04-06 00:28:00 +0100

    add isLogin middleware to activateAccount

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -3,7 +3,7 @@ import { register, activateAccount, login } from '../controllers/auth';
 import isLogin from '../middlewares/isLogin';
 const router: Router = express.Router();
 router.post('/register', register);
-router.post('/activateAccount', activateAccount);
+router.post('/activateAccount', isLogin, activateAccount);
 router.post('/activate', isLogin, activateAccount);
 router.post('/login', login);
 module.exports = router;
