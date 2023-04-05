3d6ae43c51ef17ee2f5ba7e43987804b017b4a8a
Aymanktari <aymanktari31@gmail.com>
2023-04-05 14:32:44 +0100

    sideBar

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../../components/header';
+import SideBar from '../../components/home/sidebar';
+import { useSelector } from 'react-redux';
+import RightHome from '../../components/home/right';
 
 function Home() {
+  const { user } = useSelector((state: any) => ({ ...state }));
   return (
     <div>
       <Header page='home' />
+      <SideBar user={user} />
+      <RightHome user={user} />
     </div>
   );
 }
