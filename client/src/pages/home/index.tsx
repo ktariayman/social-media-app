3e2477b5b719db7b3fa1ca6f51f01537021c05ad
Aymanktari <aymanktari31@gmail.com>
2023-04-05 14:50:26 +0100

    add stories ( design only )

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from '../../components/header';
 import SideBar from '../../components/home/sidebar';
 import { useSelector } from 'react-redux';
 import RightHome from '../../components/home/right';
-
+import Stories from '../../components/home/stories';
+import './style.css';
 function Home() {
+  const middle = useRef(null);
   const { user } = useSelector((state: any) => ({ ...state }));
   return (
-    <div>
+    <div className='home'>
       <Header page='home' />
       <SideBar user={user} />
       <RightHome user={user} />
+      <div className='home_middle' ref={middle}>
+        <Stories />
+      </div>
     </div>
   );
 }
