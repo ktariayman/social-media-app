637adcf6d1fd9ded40029eaf61f788658c1e3235
Aymanktari <aymanktari31@gmail.com>
2023-04-07 22:29:32 +0100

    add sendResetPasswordCode

--- server/controllers/auth/index.ts
+++ server/controllers/auth/index.ts
@@ -3,5 +3,12 @@ import register from './register';
 import login from './login';
 import sendVerification from './sendVerification';
 import findUserByEmail from './findUserByEmail';
-
-export { register, activateAccount, login, sendVerification, findUserByEmail };
+import sendResetPasswordCode from './sendResetPasswordCode';
+export {
+  register,
+  activateAccount,
+  login,
+  sendVerification,
+  findUserByEmail,
+  sendResetPasswordCode
+};
---
+++ server/controllers/auth/sendResetPasswordCode.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import User from '../../model/user';
+import Code from '../../model/code';
+import { sendResetCode } from '../../helper/mailer';
+import generateCode from '../../helper/generateCode';
+
+async function sendResetPasswordCode(req: Request, res: Response) {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email }).select('-password');
+    await Code.findOneAndRemove({ user: user._id });
+    const code: number = generateCode(5);
+    const savedCode = await new Code({
+      code,
+      user: user._id
+    }).save();
+    sendResetCode(user.email, user.first_name, code);
+    return res.status(200).json({
+      message: 'Email reset code has been sent to your email'
+    });
+  } catch (error) {
+    const errorMessage: string = (error as Error).message;
+    res.status(500).json({ message: errorMessage });
+  }
+}
+export default sendResetPasswordCode;
