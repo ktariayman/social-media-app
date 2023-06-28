0e0050ee88ca2d3eaf53bf8bfec0d4e2580b4b1b
Aymanktari <aymanktari31@gmail.com>
2023-06-28 15:57:42 +0100

    modify getAllPosts :  poupulate user from everyPost

--- server/controllers/post/getAllPosts.ts
+++ server/controllers/post/getAllPosts.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Post } from '../../model';
 const getAllPosts = async (req: Request, res: Response) => {
   try {
-    const posts = await Post.find( {});
+    const posts = await Post.find().populate("user","first_name last_name picture username gender");
     res.json(posts);
   } catch (error) {
     const errorMessage: string = (error as Error).message;
